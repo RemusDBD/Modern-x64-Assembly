09d3f9fbcde30a95096e9f3c76416c3d5d5ec873
Remus <127229497+RemusDBD@users.noreply.github.com>
2024-08-20 04:04:20 +0800

    Add files via upload

---
+++ 2-16 Bit Registers/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+extern "C" void SomeFunction();
+
+int main() {
+	SomeFunction();
+	
+	return 0;
+}
---
+++ 2-16 Bit Registers/asm.asm
@@ -0,0 +1,9 @@
+.code
+SomeFunction proc
+	push rbx
+	mov bx, 29
+	mov bh, 67
+	pop rbx
+	ret
+SomeFunction endp
+end
