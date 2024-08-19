2f4b882fed054880d0e0903c4c843fc3d89c77d1
Remus <127229497+RemusDBD@users.noreply.github.com>
2024-08-20 04:06:56 +0800

    Add files via upload

---
+++ 3-32 and 64 bit Registers/asm.asm
@@ -0,0 +1,17 @@
+.code
+RegTest proc
+	mov rax, -1
+
+	mov al, 5
+	mov ax, 17
+
+	mov eax, 1
+
+	mov r11, -1
+	mov r11b, 5
+	mov r11w, 17
+	mov r11d, 1
+
+	ret
+RegTest endp
+end
---
+++ 3-32 and 64 bit Registers/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+extern "C" void RegTest();
+
+int main() {
+	RegTest();
+
+	return 0;
+}
